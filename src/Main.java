735e0c23861aa9d305299b7aed73515a13806f91
exx <superjamrs323@gmail.com>
2020-11-17 00:16:24 -0700

    added deck test to main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        var myDeck = new Deck(true);
+        System.out.println(myDeck.getCards().toString());
     }
 }
