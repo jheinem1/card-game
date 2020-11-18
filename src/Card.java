eaeb572947d3017fc502687fe1ace04dd30cbfbe
exx <superjamrs323@gmail.com>
2020-11-17 19:39:41 -0700

    fixed debugging stuff

--- src/Card.java
+++ src/Card.java
@@ -3,8 +3,8 @@ public class Card {
     public enum Suit {CLUBS, DIAMONDS, HEARTS, SPADES};
     public enum Rank {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
 
-    private Suit suit;
-    private Rank rank;
+    private final Suit suit;
+    private final Rank rank;
 
     public Card(Rank rank, Suit suit) {
         this.rank = rank;
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         var myDeck = new Deck(true);
-        System.out.println(myDeck.getCards().toString());
+        System.out.println(myDeck);
+        myDeck.takeCard(Card.Rank.ACE, Card.Suit.CLUBS);
+        System.out.println(myDeck);
+        myDeck.addCard(new Card(Card.Rank.ACE, Card.Suit.CLUBS));
     }
 }
